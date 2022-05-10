caa9413b9ecd822daa52b5161cf8a5d12bf85fe6
Katrine Christensen <katch@itu.dk>
2022-05-10 09:22:59 +0200

    Wont work without forcerefresh

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import './App.css';
 import React from "react"
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from "react-router-dom"
+import {BrowserRouter, Route, Switch, NavLink} from "react-router-dom"
 import ProductList from './components/productList';
 import ProductDetails from './components/productDetails';
-import { CardDeck, Card, Container, Button, Navbar } from "react-bootstrap";
-import Product from './components/product';
-import NavigationBar from './components/NavigationBar';
-
-
+import {withRouter} from 'react-router';
+import { Nav, Navbar } from "react-bootstrap";
 
 
 class App extends React.Component {
 
   render(){ 
     const productTest = {"productId":1,"productName":"Sencha tea","categories":["green","conventional","30-50 DKK"],"price":"32 dkk","image":"/img/greenteas/sencha.jpg","description":"Sencha is a mild and soft green tea from China. It is often used in various green tea blends because it is mild and can therefore easily be combined with various ingredients such as fruits, berries and herbs."};
-    const categoriesTest = [{"categoryName":"greentea"}, {"categoryName":"blacktea"}, {"categoryName":"whitetea"}];
+    const categoriesTest = [{"categoryName":"Green tea"}, {"categoryName":"Black tea"}, {"categoryName":"White tea"},{"categoryName":"Herbal tea"},{"categoryName":"Rooibos tea"}];
 
     return (
     <div className="App">
        <BrowserRouter>
+        <div>
+                    <div id="logo-top-container"><a className="navbar-brand-top" href="/">
+                        <img src="/img/Logo.png" alt="logo"/>
+                      </a></div>
 
-         <NavigationBar categories={categoriesTest}/>
-        
-            <Switch>
-              <Route path="/greentea" component={GreenTea} />
-              <Route path="/products/:productId" component={() => <ProductDetails product={productTest}/>} />
-              <Route path="/" component={Home} />
-            </Switch>
+                    <div className="navbar navbar-expand-lg navbar-light">
+                      <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                        {categoriesTest.map((category)=>(
+                              <li className="nav-item" key={category.categoryName}>
+                              <NavLink className="nav-link" exact activeClassName="active" to={`/${category.categoryName.replaceAll(" ","")}`}>
+                                {category.categoryName}
+                              </NavLink>
+                            </li>
+                        ))}
+                      </ul>
+                    </div>
+
+                    <div id="icons-nav">
+                      <div className="search"><a type="submit" href="/search">
+                          <img src="/img/icons/search.png" alt="search"/>
+                    </a></div>
+
+                    <div className="Login"><a href="login">
+                        <img src="/img/icons/profile.png" alt="login"/>
+                    </a></div>
+
+                    <div className="Basket"><div> 
+                      <a href="/basket"><img src="/img/icons/basket.png" alt="basket"/> </a></div>
+                    <a href="/basket"><div id="basketSize"></div></a></div>
+                    </div>
+                    </div>
+              <Switch>
+                <Route path="/:category" component={GreenTea} /> {/* change later*/}
+                <Route path="/products/:productId" component={() => <ProductDetails product={productTest}/>} />
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </div>
         </BrowserRouter>
     </div>
   )};
