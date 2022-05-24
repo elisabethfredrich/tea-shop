714c258d96288a29b331562e3602b2a6980dac51
sofie nielsen <soni@itu.dk>
2022-05-24 14:26:48 +0200

    make post method to post basket and product

--- src/components/product.js
+++ src/components/product.js
@@ -2,16 +2,33 @@ import { Card, Button } from "react-bootstrap";
 import React from "react";
 import {Link} from "react-router-dom"
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const Product = ({product}) => {
 
   let history = useHistory();
   
+
   function moveToProductDetails(productId){
       history.push(`/products/${productId}`)
   }
 
-  function moveProductToBasket(productId){
+  const[userId, setUserId] = useState('1000');
+  const[products, setProducts] = useState('');
+
+  const handleClickAndMakeBasket = (productId) => {
+    const basket = {userId, products:[{productId}]};
+
+    fetch('http://localhost:9000/baskets', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(basket)
+    }).then(() =>{
+      console.log(JSON.stringify(basket))
+    })
+  }
+
+  /*function moveProductToBasket(productId){
    fetch(`http://localhost:9000/baskets/${productId}/products`,{
       mode: 'no-cors',
       method:'POST', 
@@ -20,7 +37,7 @@ const Product = ({product}) => {
     }).then(()=>{
       console.log('Product is added to basket')
     })
-  }
+  }*/
   
     return (
         <Card className="card" style={{ minWidth: '300px', border: 'none' }}>
@@ -29,7 +46,7 @@ const Product = ({product}) => {
             <Card.Title>{product.productName}</Card.Title>
             <Card.Text>{product.price}</Card.Text>
             <Link to="/" className="btn">Home</Link>
-            <Button onClick={() => moveProductToBasket(product.productId)}>Add to basket</Button>
+            <Button onClick={() => handleClickAndMakeBasket(product.productId)}>Add to basket</Button>
             <Button onClick={() => moveToProductDetails(product.productId)}>Read more</Button>
 
             {/* Another way to make the buttons: */}
