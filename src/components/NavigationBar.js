7164f92da9a50c6d02bf2d93fdafce16644dcc1e
Merle <mers@itu.dk>
2022-05-24 15:07:08 +0200

    reactive navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,20 +1,19 @@
 import {Link} from "react-router-dom"
 import React from "react";
 
-
 const NavigationBar = (categories) => {
   const categoriesTest = [{"categoryName":"Green tea"}, {"categoryName":"Black tea"}, {"categoryName":"White tea"},{"categoryName":"Herbal tea"},{"categoryName":"Rooibos tea"}];
-
   return(
     <div>
       <div id="logo-top-container"><Link className="navbar-brand-top" to="/">
           <img src="/img/Logo.png" alt="logo"/>
         </Link></div>
-
       <div className="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
-
       
           {categoriesTest.map((category)=>(
                 <li className="nav-item" key={category.categoryName}>
@@ -26,16 +25,13 @@ const NavigationBar = (categories) => {
           ))}
         </ul>
       </div>
-
       <div id="icons-nav">
         <div className="search"><Link type="submit" to="/search">
             <img src="/img/icons/search.png" alt="search"/>
       </Link></div>
-
       <div className="Login"><Link to="/login">
           <img src="/img/icons/profile.png" alt="login"/>
       </Link></div>
-
       <div className="Basket"><div> 
         <Link to="/basket"><img src="/img/icons/basket.png" alt="basket"/> </Link></div>
       <Link to="/basket"><div id="basketSize"></div></Link></div>
@@ -43,5 +39,4 @@ const NavigationBar = (categories) => {
       </div>
 </div>
     )}
-
 export default NavigationBar;
